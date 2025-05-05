340977e69c9c4c4e65fe7fe9e0481d045c664a99
Clement_127001 <babiyonclement18@gmail.com>
2025-05-05 11:04:31 +0530

    add initial setup for server

---
+++ server/middleware/error-handler.js
@@ -0,0 +1,30 @@
+const { StatusCodes } = require("http-status-codes");
+const errorHandlerMiddleware = (err, req, res, next) => {
+  let customError = {
+    statusCode: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
+    msg: err.message || "Something went wrong, please try again later",
+  };
+
+  if (err.name === "CastError") {
+    customError.statusCode = StatusCodes.BAD_REQUEST;
+    customError.msg = `No item found with id : ${err.value}`;
+  }
+
+  if (err.name === "ValidationError") {
+    customError.statusCode = StatusCodes.BAD_REQUEST;
+    customError.msg = Object.values(err.errors)
+      .map((item) => item.message)
+      .join(",");
+  }
+
+  if (err.code && err.code == 11000) {
+    customError.statusCode = StatusCodes.BAD_REQUEST;
+    customError.msg = `${Object.keys(
+      err.keyValue
+    )} already exists, please choose another email`;
+  }
+
+  res.status(customError.statusCode).json({ msg: customError.msg });
+};
+
+module.exports = errorHandlerMiddleware;
